fbaa0a2e130ecfb55439bd0d83adfc3a3c923dd4
Nikki <nikki@nicolahibbert.com>
2010-12-28 19:25:50 +0000

    tidied up js

--- liteTabs.jQuery.js
+++ liteTabs.jQuery.js
@@ -10,75 +10,81 @@
 		http://creativecommons.org/licenses/by/3.0/
 
 /*******************************************/
-jQuery.fn.liteTabs = function(options) {
-	return this.each(function() {
-		var lt = $(this),
-			ul = lt.children('ul'),
-			tab = ul.find('a'),
-			div = lt.children('div'),
-			opts = jQuery.extend({
-				borders : false,
-				boxed : false,
-				colourScheme : false,
-				fadeIn : false,
-				height : 'auto',
-				hideHash : false,
-				rounded : false,
-				selectedTab : 1,
-				width : 500
-			}, options);
 
-		// set liteTabs class for css
-		lt.addClass('liteTabs');
+(function($) {
 
-		// option: set overall height
-		div.addClass('pos').height(opts.height).width(opts.width - 20);
+	$.fn.liteTabs = function(options) {
+		var lt = this,
+		ul = lt.children('ul'),
+		tab = ul.find('a'),
+		div = lt.children('div'),
+		opts = jQuery.extend({
+			borders : false,
+			boxed : false,
+			colourScheme : false,
+			fadeIn : false,
+			height : 'auto',
+			hideHash : false,
+			rounded : false,
+			selectedTab : 1,
+			width : 500
+		}, options);
 
-		// option: set overall width
-		lt.width(opts.width);
+		return this.each(function() {
 
-		// on tab click...
-		tab.click(function(e) {
+			// set liteTabs class for css
+			lt.addClass('liteTabs');
 
-			// default: add selected class to tab
-			tab.removeClass('selected').filter(this).addClass('selected');
+			// option: set overall height
+			div.addClass('pos').height(opts.height).width(opts.width - 20);
 
-			// option: fade in divs
-			if (opts.fadeIn) {
-				div.removeClass('selected').filter('[name=' + this.hash + ']').hide().addClass('selected').fadeIn();
-			} else {
-				// default: add selected class to div
-				div.removeClass('selected').filter('[name=' + this.hash + ']').addClass('selected');
+			// option: set overall width
+			lt.width(opts.width);
+
+			// on tab click...
+			tab.click(function(e) {
+
+				// default: add selected class to tab
+				tab.removeClass('selected').filter(this).addClass('selected');
+
+				// option: fade in divs
+				if (opts.fadeIn) {
+					div.removeClass('selected').filter('[name=' + this.hash + ']').hide().addClass('selected').fadeIn();
+				} else {
+					// default: add selected class to div
+					div.removeClass('selected').filter('[name=' + this.hash + ']').addClass('selected');
+				}
+
+				// option: hide hash change
+				if (opts.hideHash) {
+					e.preventDefault();
+				}
+
+			});
+
+			// option: set selected tab
+			if (opts.selectedTab) {
+				tab.eq(--opts.selectedTab).click();
 			}
 
-			// option: hide hash change
-			if (opts.hideHash) {
-				e.preventDefault();
+			// option: set rounded corners
+			if (opts.rounded) {
+				lt.addClass('rounded');
 			}
+
+			// option: set borders
+			if (opts.borders) {
+				lt.addClass('borders');
+				div.width(div.width() - 2);	// need to fix this
+			}
+
+			if (opts.boxed) {
+				lt.addClass('boxed');
+			}
+
+			return lt;
 
 		});
+	};
 
-		// option: set selected tab
-		if (opts.selectedTab) {
-			tab.eq(--opts.selectedTab).click();
-		}
-
-		// option: set rounded corners
-		if (opts.rounded) {
-			lt.addClass('rounded');
-		}
-
-		// option: set borders
-		if (opts.borders) {
-			lt.addClass('borders');
-			div.width(div.width() - 2);	// need to fix this
-		}
-
-		if (opts.boxed) {
-			lt.addClass('boxed');
-		}
-
-		return lt;
-
-	});
-};
+})(jQuery);
